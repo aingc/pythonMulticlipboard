9d681a5527038f8b10d727c55ff30255c4eda2d4
Corey <aingc@users.noreply.github.com>
2023-05-12 15:54:47 -0700

    Save and Load CLI command

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,22 +2,39 @@ import sys
 import clipboard
 import json
 
-def saveItems(filepath, data):
+SAVED_DATA = "clipboard.json"
+
+def saveData(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
-def loadJson(filepath):
-    with open(filepath, "r") as f:
-        data = json.load(f)
-        return data
+def loadData(filepath):
+    # handle err if file doesn't exist
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
+    # attempt to load file
+    data = loadData(SAVED_DATA)
 
     if command == "save":
-        print("save")
+        key = input("Enter a key to save: ")
+        # store inside of data dictionary
+        data[key] = clipboard.paste()
+        saveData(SAVED_DATA, data)
+        print("Data saved.")
     elif command == "load":
-        print("load")
+        key = input("Enter a key to load: ")
+        if key in data:
+            clipboard.copy(data[key])
+            print("Data copied to clipboard.")
+        else:
+            print("Key does not exist.")
     elif command == "list":
         print("list")
     else:
